b91170255a68d14ffce0435e6e62d4a500a6d461
Jaclyn Williams <jaclyn.williams143@gmail.com>
2023-01-04 11:35:51 -0500

    commit tree 1ee3777a13786de4c05260af5f394076c9b71675

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+/* FIRST ATTEMPT: Passed first test 
+const totalBatteries = batteryBatches.reduce(function(countObj, battery) {
+    if (battery in countObj) {
+        countObj[battery]++;
+    } else {
+        countObj[battery] = 1;
+    }
+    return countObj;
+}, {});
+*/
+
+/* SECOND ATTEMPT: also passed first test
+const totalBatteries = batteryBatches.reduce(function(accumulator, battery){
+    return battery + accumulator}, {})
+    */
+   
+/*THIRD ATTEMPT: Passed first 2 tests
+ const initalValue = 4;
+ const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => accumulator + currentValue, initalValue
+); */
+
+const initalValue = 0;
+const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => accumulator += currentValue, initalValue
+); 
+
